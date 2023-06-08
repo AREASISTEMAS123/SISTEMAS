61554de5cf964c077bf6cf8360ed89430e872912
anderssongodoy <anderssongodoygarica@gmail.com>
2023-06-08 08:28:24 -0500

    Cumpleaños Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './Components/Login.jsx'
 import './css/index.css'
+import Calendar from './Components/Cumpleaños.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
+      <Calendar/>
+    {/* <Login/> */}
   </React.StrictMode>,
 )
