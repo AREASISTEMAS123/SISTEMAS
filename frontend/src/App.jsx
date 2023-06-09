aebbb78bd0ed15dafb403ddc2ecfe989b5a96c5e
Arturo Antonio Montejo Soto <121409768+AntonioSotoAS@users.noreply.github.com>
2023-06-09 09:39:24 -0500

    colaboradorPerfil responsive-web card info

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import { PerfilColaborador } from "./Components/PerfilColaborador";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <AppRoutes />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <AppRoutes />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
