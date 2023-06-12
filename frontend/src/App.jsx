925face49126f2fd29a81873b77a60632a5b060a
Arturo Antonio Montejo Soto <121409768+AntonioSotoAS@users.noreply.github.com>
2023-06-12 13:03:10 -0500

    menu resposive-celular

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
-import { Sidebar } from './components/commons/Sidebar';
 import { Topbar } from './components/commons/Topbar';
+import Sidebar from './Components/commons/Sidebar';
+
+
 
 const App = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
 
-    const toggleSidebar = () => {
-        setSidebarOpen(!sidebarOpen);
+
+ 
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
     };
 
-    return (
-        <BrowserRouter>
-            <div className="flex h-screen overflow-hidden">
-                <Sidebar isOpen={sidebarOpen} />
-                <div className="flex flex-col flex-1">
-                    <Topbar toggleSidebar={toggleSidebar} />
-                    <div className="flex flex-col flex-1 overflow-y-auto">
-                        <AppRoutes />
-                    </div>
-                </div>
-            </div>
-        </BrowserRouter>
-    );
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="flex h-screen overflow-hidden">
+        {!isMobile && <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />}
+        <div className="flex flex-col flex-1">
+          <Topbar toggleSidebar={toggleSidebar} />
+          <div className="flex flex-col flex-1 overflow-y-auto">
+            <AppRoutes />
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
