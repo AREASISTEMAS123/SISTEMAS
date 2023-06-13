7067bb010720d60f38638bbe235778345e695d17
Anaiz Rojas <ani.rojas.sen@gmail.com>
2023-06-13 12:40:27 -0500

    actualizacion-login

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Cumpleanos, Login, RegistroAsistencia, PerfilColaborador, VistaEvaluaciones, VistaHomeColaborador, JustificacionColaborador } from '../components';
+import { Cumpleanos, Login, RegistroAsistencia, PerfilColaborador, VistaEvaluaciones, VistaHomeColaborador, JustificacionColaborador, AdmiJustificacion } from '../components';
 
 function AppRoutes() {
     return (
@@ -11,7 +11,7 @@ function AppRoutes() {
             <Route path="/asistencia" element={<RegistroAsistencia />} />
             <Route path="/perfil" element={<PerfilColaborador />} />
             <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
-            <Route path="/justificaciones" element={<JustificacionColaborador/>}/>
+            <Route path="/justificaciones" element={<AdmiJustificacion/>}/>
         </Routes>
     );
 }
