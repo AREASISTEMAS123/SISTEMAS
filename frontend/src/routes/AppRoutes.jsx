c8554bb50a62bec46d8ed28d627743303e5e4c17
CristianVasquez13 <vasquezcristian140@gmail.com>
2023-06-13 09:49:29 -0500

    Corrigiendo AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Cumpleanos, Login, RegistroAsistencia, PerfilColaborador, VistaEvaluaciones, VistaHomeColaborador, AdmiJustificacion, JustificacionColaborador } from '../components';
+import { Cumpleanos, Login, RegistroAsistencia, PerfilColaborador, VistaEvaluaciones, VistaHomeColaborador, JustificacionColaborador } from '../components';
 
 function AppRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<VistaHomeColaborador />} />
             <Route path="/cumpleanos" element={<Cumpleanos />} />
             <Route path="/login" element={<Login />} />
             <Route path="/asistencia" element={<RegistroAsistencia />} />
