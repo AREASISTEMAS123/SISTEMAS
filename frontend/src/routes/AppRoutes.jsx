996f40d86322add0b9447919bb07612a848cc34c
anderssongodoy <anderssongodoygarica@gmail.com>
2023-06-15 12:43:47 -0500

    Rutas

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import {
-  Cumpleanos,
-  Login,
-  RegistroAsistencia,
-  PerfilColaborador,
-  VistaEvaluaciones,
-  VistaHomeColaborador,
+    Cumpleanos,
+    Login,
+    RegistroAsistencia,
+    PerfilColaborador,
+    VistaEvaluaciones,
+    VistaHomeColaborador,
+    AsistenciaAdmin,
+    AdmiJustificacion,
 } from "../components";
-import { AsistenciaAdmin } from "../Components/AsistenciaAdmin";
 
 function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<VistaHomeColaborador />} />
-      <Route path="/cumpleanos" element={<Cumpleanos />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/asistencia" element={<RegistroAsistencia />} />
-      <Route path="/perfil" element={<PerfilColaborador />} />
-      <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
-      <Route path="/asistenciaAdmin" element={<AsistenciaAdmin />} />
-      <Route path="/justificaciones" element={<AsistenciaAdmin />} />
-      <Route path="/homeColaborador" element={<VistaHomeColaborador />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<VistaHomeColaborador />} />
+            <Route path="/cumpleanos" element={<Cumpleanos />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/asistencia" element={<RegistroAsistencia />} />
+            <Route path="/perfil" element={<PerfilColaborador />} />
+            <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
+            <Route path="/asistenciaAdmin" element={<AsistenciaAdmin />} />
+            <Route path="/justificaciones" element={<AdmiJustificacion />} />
+            <Route path="/homeColaborador" element={<VistaHomeColaborador />} />
+        </Routes>
+    );
 }
 
 export default AppRoutes;
