ebf98ebe2ba1879f163f9bd3fc561a215d075cd5
Alejandro De La Cruz <97196517+DevLaCruz@users.noreply.github.com>
2023-06-24 09:41:35 -0500

    Update Evaluation.php

--- backend/app/Models/Evaluation.php
+++ backend/app/Models/Evaluation.php
@@ -1,1 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+   
+    public $timestamps = false;
+    protected $fillable = ['id', 'user_id', 'model_type'];
+
+    
+public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user','id');
+    }
+
+}
+
 
