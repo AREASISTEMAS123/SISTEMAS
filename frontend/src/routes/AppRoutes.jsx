c1a7f06b8a6a7f9c9a0deb8f1af7cec1334674fe
anderssongodoy <anderssongodoygarica@gmail.com>
2023-06-28 11:34:26 -0500

    Seguridad de Enrutamiento

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import {
     Cumpleanos,
     Login,
@@ -12,28 +12,69 @@ import {
     JustificacionColaborador,
     EvaluacionesColaborador
 } from "../components";
-import { VistaReportes } from "../Components/VistaReportes";
-import {OlvideContraseña} from "../Components/OlvideContraseña"
-import {RestablecerContraseña} from "../Components/RestablecerContraseña"
-function AppRoutes() {
+
+const AppRoutes = () => {
+    const rol = localStorage.getItem('rol');
+
+    // Función para verificar si el usuario tiene un rol específico
+    const hasRole = (targetRole) => {
+        return rol === targetRole;
+    };
+
     return (
         <Routes>
             <Route path="/" element={<VistaHomeColaborador />} />
-            <Route path="/cumpleanos" element={<Cumpleanos />} />
+
+            {hasRole('Colaborador') && (
+                <>
+                    <Route path="/cumpleanos" element={<Cumpleanos />} />
+                    <Route path="/detalleCumpleanos/:month/:day" element={<DetalleCumpleanos />} />
+                    <Route path="/perfil" element={<PerfilColaborador />} />
+                    <Route path="/asistencia" element={<RegistroAsistencia />} />
+                    <Route path="/justificacion" element={<JustificacionColaborador />} />
+                    <Route path="/evaluacion" element={<EvaluacionesColaborador />} />
+                </>
+            )}
+
+            {hasRole('Lider Area') && (
+                <>
+                    <Route path="/cumpleanos" element={<Cumpleanos />} />
+                    <Route path="/detalleCumpleanos/:month/:day" element={<DetalleCumpleanos />} />
+                    <Route path="/perfil" element={<PerfilColaborador />} />
+                    <Route path="/asistencia" element={<RegistroAsistencia />} />
+                    <Route path="/justificacion" element={<JustificacionColaborador />} />
+                    <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
+                </>
+            )}
+
+            {hasRole('Lider Departamento') && (
+                <>
+                    <Route path="/cumpleanos" element={<Cumpleanos />} />
+                    <Route path="/detalleCumpleanos/:month/:day" element={<DetalleCumpleanos />} />
+                    <Route path="/perfil" element={<PerfilColaborador />} />
+                    <Route path="/asistencia" element={<RegistroAsistencia />} />
+                    <Route path="/justificacion" element={<JustificacionColaborador />} />
+                    <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
+                </>
+            )}
+
+            {hasRole('Gerencia') && (
+                <>
+                    <Route path="/cumpleanos" element={<Cumpleanos />} />
+                    <Route path="/detalleCumpleanos/:month/:day" element={<DetalleCumpleanos />} />
+                    <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
+                    <Route path="/asistenciaAdmin" element={<AsistenciaAdmin />} />
+                    <Route path="/justificaciones" element={<AdmiJustificacion />} />
+                    <Route path="/reportes" element={<VistaReportes />} />
+                </>
+            )}
+
             <Route path="/login" element={<Login />} />
-            <Route path="/asistencia" element={<RegistroAsistencia />} />
-            <Route path="/perfil" element={<PerfilColaborador />} />
-            <Route path="/evaluaciones" element={<VistaEvaluaciones />} />
-            <Route path="/asistenciaAdmin" element={<AsistenciaAdmin />} />
-            <Route path="/justificaciones" element={<AdmiJustificacion />} />
-            <Route path="/justificacionColaborador" element={<JustificacionColaborador />} />
-            <Route path="/homeColaborador" element={<VistaHomeColaborador />} />
-            <Route path="/detalleCumpleanos/:month/:day" element={<DetalleCumpleanos />} />
-            <Route path="/reportes" element={<VistaReportes />} />
-            <Route path="/OlvideContraseña" element = {<OlvideContraseña/>}/>
-            <Route path="/RestablecerContraseña" element = {<RestablecerContraseña/>}/>
+
+            {/* Ruta por defecto para usuarios con roles no definidos */}
+            <Route path="/*" element={<Navigate to="/login" />} />
         </Routes>
     );
-}
+};
 
 export default AppRoutes;
