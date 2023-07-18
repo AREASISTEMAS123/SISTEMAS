07878f6f8b2d212cc442541e3067864abbe10416
Caleb <reateguicevallosc@gmail.com>
2023-07-18 12:51:22 -0500

    Update en Attendance Controller
    Finish insert function & Add validations

--- backend/app/Http/Controllers/AttendanceController.php
+++ backend/app/Http/Controllers/AttendanceController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Attendance;
+use App\Models\Profile;
 
 class AttendanceController extends Controller
-{
+{   
+    
     public function getattendance(){
         $attendance_user = Attendance::all();
 
@@ -14,35 +16,71 @@ class AttendanceController extends Controller
     }
 
     public function insertAttendance(Request $request) {
+        $user_id = auth()->id();
         
-        #$user_id = auth()->id();
+        // Obtener el registro de asistencia del usuario para la fecha actual
+        $attendance = Attendance::where('user_id', $user_id) 
+            ->whereDate('date', date('Y-m-d')) 
+            ->first();
+        
+        if (empty($attendance) == 1) {
 
-        #$validacion = Attendance::find($user_id);
+            // No existe un registro de asistencia para la fecha actual, crear uno nuevo
 
-        $validacion = 0;
-
-        if ($validacion != 0) {
-
-            #LOGICA DE UPDATE DATA (DEPARTURE_TIME & DEPARTURE_IMAGE)
-
-        } else {
-            
-            #ES UN MARCADO DE ENTRADA
-            
             $attendance = new Attendance();
-
+    
             $attendance->date = $request->input('date');
             $attendance->admission_time = $request->input('admission_time');
-            $attendance->user_id = auth()->id();
-            $attendance->attendance = 1;
 
+            // Recogemos los datos del turno del usuario logeado (shift)
+
+            $user_profile = Profile::where('user_id', $user_id)->get();
+
+            // Si el turno es tarde, la hora de entrada es a las 14:11:00, si es mañana, a las 08:11:00
+
+            $user_profile[0]['shift'] = 'Tarde' ? $hora = '14:11:00' : $hora = '08:11:00';
+            
+            // Aumentamos los valores de asistencia o tardanza dependiendo de la condicional
+
+            if ($attendance->admission_time >= $hora){
+                $attendance->delay = 1;
+            } else {
+                $attendance->attendance = 1;    
+            }
+
+            $attendance->user_id = $user_id;
+    
             if ($request->hasFile("admission_image")) {
                 $file = $request->file("admission_image");
                 $attendance->admission_image = $file->getClientOriginalName();
             }
-
+    
             $attendance->save();
+    
             return response()->json(['attendance' => $attendance]);
+            
+        } else {
+
+            // Existe un registro de asistencia para la fecha actual
+
+            if (is_null($attendance->departure_time)) {
+
+                // Actualizar el registro existente con la hora de salida
+
+                $attendance->departure_time = $request->input('departure_time');
+        
+                if ($request->hasFile("departure_image")) {
+                    $file = $request->file("departure_image");
+                    $attendance->departure_image = $file->getClientOriginalName();
+                }
+
+                $attendance->save();
+                return response()->json(['attendance' => $attendance]);
+
+            } else {
+                // Retornar error de marcado de asistencia
+                return response()->json(['error' => 'Ya marcaste asistencia']);
+            }
         }
     }
 }
