0fad05c83e2945f81c28e14655a64ccac3975afd
Caleb <reateguicevallosc@gmail.com>
2023-07-19 12:46:56 -0500

    Update Attendance Controller
    Guardado de imagenes en el repositorio
    Validacion de marcado de salida

--- backend/app/Http/Controllers/AttendanceController.php
+++ backend/app/Http/Controllers/AttendanceController.php
@@ -7,17 +7,25 @@ use App\Models\Attendance;
 use App\Models\Profile;
 
 class AttendanceController extends Controller
+{
 
-    public function getAttendance(){
+    public function getAttendance()
+    {
         $attendance_user = Attendance::with('user', 'profile')->get();
 
         return response()->json(['attendance' => $attendance_user]);
     }
 
+    # ----------------------- AUTORES ----------------------------- #
+    # -------------- Jose Casma y Caleb Reategui ------------------ #
+    # ---------------------- PARAMETROS --------------------------- #
+    # ------------------------- && -------------------------------- #
+    # -------------------- FUNCIONALIDAD -------------------------- #
+    # --------- Insertar asistencias de los colaboradores --------- #
 
-
-
-    public function insertAttendance(Request $request) {
+    public function insertAttendance(Request $request)
+    {   
+        //Recogemos el ID del usuario logeado
         $user_id = auth()->id();
 
         // Obtener el registro de asistencia del usuario para la fecha actual
@@ -25,64 +33,104 @@ class AttendanceController extends Controller
             ->whereDate('date', date('Y-m-d'))
             ->first();
 
+        // Recogemos los datos del turno del usuario logeado (shift)
+        $user_profile = Profile::where('user_id', $user_id)->get();
+
+        // Si el turno es tarde, la hora de salida es a las 19:00:00, si es mañana, a las 13:00:00
+        $departure_hour = $user_profile[0]['shift'] = 'Tarde' ? '19:01:00' : '13:01:00';
+
         if (empty($attendance) == 1) {
 
             // No existe un registro de asistencia para la fecha actual, crear uno nuevo
-
             $attendance = new Attendance();
 
+            //Recogemos los valores de fecha y tiempo de marcado de entrada
             $attendance->date = $request->input('date');
             $attendance->admission_time = $request->input('admission_time');
 
-            // Recogemos los datos del turno del usuario logeado (shift)
-
-            $user_profile = Profile::where('user_id', $user_id)->get();
-
             // Si el turno es tarde, la hora de entrada es a las 14:11:00, si es mañana, a las 08:11:00
-
             $user_profile[0]['shift'] = 'Tarde' ? $hora = '14:11:00' : $hora = '08:11:00';
 
             // Aumentamos los valores de asistencia o tardanza dependiendo de la condicional
-
-            if ($attendance->admission_time >= $hora){
+            if ($attendance->admission_time >= $hora) {
                 $attendance->delay = 1;
             } else {
                 $attendance->attendance = 1;
             }
 
+            // Recogemos el ID del usuario logeado
             $attendance->user_id = $user_id;
 
             if ($request->hasFile("admission_image")) {
+
+                //Recogemos la imagen de entrada
                 $file = $request->file("admission_image");
+                $folderName = date("Y-m-d"); // Obtiene la fecha de hoy en formato "año-mes-día"
+                $path = "attendace/" . $folderName; // Ruta de la carpeta con la fecha de hoy
+                $filename = time() . "-" . $file->getClientOriginalName();
+                $uploadSuccess = $file->move($path, $filename);
                 $attendance->admission_image = $file->getClientOriginalName();
+                
+            } else {
+                
+                // Retornar error si la imagen no existe
+                return response()->json(['message' => 'Image is required']);
             }
 
+            //Guardamos los cambios en la base de datos
             $attendance->save();
 
+            //Retornamos la respuesta en formato JSON
             return response()->json(['attendance' => $attendance]);
 
         } else {
 
-            // Existe un registro de asistencia para la fecha actual
+            //La hora actual es mayor que la de salida, si no marcó hora de salida, se le marca falta.
+            if ($departure_hour < date('H:i:s') && is_null($attendance->departure_time )) {
 
+                //Seteamos los valores de asistencia o tardanza dependiendo de la condicional
+                $attendance->delay = 0;
+                $attendance->attendance = 0;
+                $attendance->absence = 1;
+
+                //Guardamos los cambios en la base de datos
+                $attendance->save();
+
+                //Retornamos la respuesta en formato JSON
+                return response()->json(['attendance' => $attendance]);
+            }
+
+            // Existe un registro de asistencia para la fecha actual
             if (is_null($attendance->departure_time)) {
 
                 // Actualizar el registro existente con la hora de salida
-
                 $attendance->departure_time = $request->input('departure_time');
 
                 if ($request->hasFile("departure_image")) {
-                    $file = $request->file("departure_image");
-                    $attendance->departure_image = $file->getClientOriginalName();
-                }
 
+                    //Recogemos la imagen de entrada
+                    $file = $request->file("departure_image");
+                    $folderName = date("Y-m-d"); // Obtiene la fecha de hoy en formato "año-mes-día"
+                    $path = "attendance/" . $folderName; // Ruta de la carpeta con la fecha de hoy
+                    $filename = time() . "-" . $file->getClientOriginalName();
+                    $uploadSuccess = $file->move($path, $filename);
+                    $attendance->departure_image = $file->getClientOriginalName();
+
+                } else {
+
+                    // Retornar error si la imagen no existe
+                    return response()->json(['message' => 'Image is required']);
+                }
+    
+                //Guardamos los cambios en la base de datos
                 $attendance->save();
+
+                //Retornamos la respuesta en formato JSON
                 return response()->json(['attendance' => $attendance]);
 
-            } else {
+            } else
                 // Retornar error de marcado de asistencia
                 return response()->json(['error' => 'Ya marcaste asistencia']);
-            }
         }
     }
 }
