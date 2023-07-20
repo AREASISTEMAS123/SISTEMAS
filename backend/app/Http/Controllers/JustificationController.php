e41a79e3e36758d4a1789ea7a0967e3876b64d69
Rodrigo <torres.rodrigo.752@gmail.com>
2023-07-20 10:32:32 -0500

    Update JustificationController.php

--- backend/app/Http/Controllers/JustificationController.php
+++ backend/app/Http/Controllers/JustificationController.php
@@ -88,7 +88,7 @@ class JustificationController extends Controller
     }
 
     public function detailsJustification($id){
-        $justification = Justification::with('User')->where('id', $id)->get();
+        $justification = Justification::with('User.role')->where('id', $id)->get();
     return response()->json($justification);
     }
 
