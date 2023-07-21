77096aecf6599cc0d2caa509390ccb9f51acef1c
Jacn_159 <jcasma37@gmail.com>
2023-07-21 11:09:56 -0500

    añadiendo insert con validaciones

--- backend/app/Http/Controllers/AttendanceController.php
+++ backend/app/Http/Controllers/AttendanceController.php
@@ -5,22 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Attendance;
 use App\Models\Profile;
-use App\Models\User;
-use App\Traits\AttendanceNotificationTrait;
-use Illuminate\Support\Facades\DB;
 
 class AttendanceController extends Controller
+{
 
-    public function getAttendance(){
+    public function getAttendance()
+    {
         $attendance_user = Attendance::with('user', 'profile')->get();
 
         return response()->json(['attendance' => $attendance_user]);
     }
 
+    # ----------------------- AUTORES ----------------------------- #
+    # -------------- Jose Casma y Caleb Reategui ------------------ #
+    # ---------------------- PARAMETROS --------------------------- #
+    # ------------------------- && -------------------------------- #
+    # -------------------- FUNCIONALIDAD -------------------------- #
+    # --------- Insertar asistencias de los colaboradores --------- #
 
-
-
-    public function insertAttendance(Request $request) {
+    public function insertAttendance(Request $request)
+    {
+        //Recogemos el ID del usuario logeado
         $user_id = auth()->id();
 
         // Obtener el registro de asistencia del usuario para la fecha actual
@@ -28,11 +33,18 @@ class AttendanceController extends Controller
             ->whereDate('date', date('Y-m-d'))
             ->first();
 
+        // Recogemos los datos del turno del usuario logeado (shift)
+        $user_profile = Profile::where('user_id', $user_id)->get();
+
+        // Si el turno es tarde, la hora de salida es a las 19:00:00, si es mañana, a las 13:00:00
+        $departure_hour = $user_profile[0]['shift'] = 'Tarde' ? '19:01:00' : '13:01:00';
+
         if (empty($attendance) == 1) {
 
             // No existe un registro de asistencia para la fecha actual, crear uno nuevo
             $attendance = new Attendance();
 
+            //Recogemos los valores de fecha y tiempo de marcado de entrada
             $attendance->date = $request->input('date');
             $attendance->admission_time = $request->input('admission_time');
 
@@ -46,18 +58,47 @@ class AttendanceController extends Controller
                 $attendance->attendance = 1;
             }
 
+            // Recogemos el ID del usuario logeado
             $attendance->user_id = $user_id;
 
             if ($request->hasFile("admission_image")) {
+
+                //Recogemos la imagen de entrada
                 $file = $request->file("admission_image");
+                $folderName = date("Y-m-d"); // Obtiene la fecha de hoy en formato "año-mes-día"
+                $path = "attendace/" . $folderName; // Ruta de la carpeta con la fecha de hoy
+                $filename = time() . "-" . $file->getClientOriginalName();
+                $uploadSuccess = $file->move($path, $filename);
                 $attendance->admission_image = $file->getClientOriginalName();
+
+            } else {
+
+                // Retornar error si la imagen no existe
+                return response()->json(['message' => 'Image is required']);
             }
 
+            //Guardamos los cambios en la base de datos
             $attendance->save();
 
+            //Retornamos la respuesta en formato JSON
             return response()->json(['attendance' => $attendance]);
 
         } else {
+
+            //La hora actual es mayor que la de salida, si no marcó hora de salida, se le marca falta.
+            if ($departure_hour < date('H:i:s') && is_null($attendance->departure_time )) {
+
+                //Seteamos los valores de asistencia o tardanza dependiendo de la condicional
+                $attendance->delay = 0;
+                $attendance->attendance = 0;
+                $attendance->absence = 1;
+
+                //Guardamos los cambios en la base de datos
+                $attendance->save();
+
+                //Retornamos la respuesta en formato JSON
+                return response()->json(['attendance' => $attendance]);
+            }
 
             // Existe un registro de asistencia para la fecha actual
             if (is_null($attendance->departure_time)) {
@@ -66,53 +107,30 @@ class AttendanceController extends Controller
                 $attendance->departure_time = $request->input('departure_time');
 
                 if ($request->hasFile("departure_image")) {
+
+                    //Recogemos la imagen de entrada
                     $file = $request->file("departure_image");
+                    $folderName = date("Y-m-d"); // Obtiene la fecha de hoy en formato "año-mes-día"
+                    $path = "attendance/" . $folderName; // Ruta de la carpeta con la fecha de hoy
+                    $filename = time() . "-" . $file->getClientOriginalName();
+                    $uploadSuccess = $file->move($path, $filename);
                     $attendance->departure_image = $file->getClientOriginalName();
+
+                } else {
+
+                    // Retornar error si la imagen no existe
+                    return response()->json(['message' => 'Image is required']);
                 }
 
+                //Guardamos los cambios en la base de datos
                 $attendance->save();
+
+                //Retornamos la respuesta en formato JSON
                 return response()->json(['attendance' => $attendance]);
 
-            } else{
+            } else
                 // Retornar error de marcado de asistencia
                 return response()->json(['error' => 'Ya marcaste asistencia']);
-            }
         }
-
-        // Logica para notificar a gerencia y el lider de departamento
-        
-        // Obtener user por ID
-        // $user = User::all()
-        //                 ->except($attendance->user_id);
-                        // ->each(function(User $user) use ($attendance){
-                            
-                        // });
-        // Calcula el numero de faltas del usuario
-        // $faltasCount = $this->calculateFaltasCount($user_id);
-
-        // Si el usuario tiene 2 faltas se envia advertencia al lider del departamento, si tiene 3 la alerta llega a gerencia
-    //     if($faltasCount === 3) {
-    //         auth()->user()->
-    //     } else if ($faltasCount === 2) {
-    //         $this->sendNotification($user, $faltasCount, 'advertencia');
-    //     }
     }
-
-    // Método para calcular el número de faltas del usuario
-    // private function calculateFaltasCount($user_id)
-    // {
-        // Obtener las asistencias del usuario
-//         $attendances = Attendance::where('user_id', $user_id)->get();
-
-//         // Contar las faltas
-//         $faltasCount = 0;
-//         foreach ($attendances as $attendance) {
-//             // Si el campo 'attendance' es false, entonces es falta
-//             if (!$attendance->attendance) {
-//                 $faltasCount++;
-//             }
-//         }
-
-//         return $faltasCount;
-//     }
 }
