f3cc1a0e0ab5182c34dafbdfb907e401c0e285a5
Alejandro De La Cruz <97196517+DevLaCruz@users.noreply.github.com>
2023-08-03 12:49:53 -0500

    Crear método para generar el reporte en AttendanceReport.php

--- backend/app/Models/AttendanceReport.php
+++ backend/app/Models/AttendanceReport.php
@@ -18,3 +18,28 @@ class AttendanceReport extends Model
         'date',
     ];
 }
+public function generarReporteGeneral(Request $request)
+{
+    // Obtener los datos de la tabla attendances
+    $attendances = Attendance::all(); 
+    // Procesar los datos y calcular los valores para el reporte
+    $reportData = []; // Inicializar un arreglo para los datos del reporte
+
+    foreach ($attendances as $attendance) {
+        // Realiza los cálculos necesarios para llenar los atributos del reporte
+        $reportData[] = [
+            'attendances' => $attendance->attendances,
+            'delays' => $attendance->delays,
+            'absences' => $attendance->absences,
+            'justifications' => $attendance->justifications,
+            'total' => $attendance->total,
+            'shift' => $attendance->shift,
+            'date' => $attendance->date,
+        ];
+    }
+
+    // Insertar los datos en la tabla attendance_reports
+    AttendanceReport::insert($reportData);
+
+    return response()->json(['message' => 'Reporte generado y llenado exitosamente']);
+}
